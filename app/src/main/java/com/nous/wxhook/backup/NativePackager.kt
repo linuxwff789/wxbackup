4b48e13565965535333c142a7511e0b7e4877c56
wxhook <wxhook@backup>
2026-07-15 11:16:26 +0800

    fix: use wxhook_bin/tar instead of termux tar

--- app/src/main/java/com/nous/wxhook/backup/NativePackager.kt
+++ app/src/main/java/com/nous/wxhook/backup/NativePackager.kt
@@ -7,13 +7,14 @@ object NativePackager {
 
     fun packageFiles(outputPath: String, backupDir: String, backupFiles: List<String>,
                      microMsgDir: String, attDirs: List<String>): Int {
-        val args = mutableListOf("/data/data/com.termux/files/usr/bin/tar",
-            "--use-compress-program", "/data/local/tmp/wxhook_bin/zstd",
+        val binDir = "/data/local/tmp/wxhook_bin"
+        val args = mutableListOf("$binDir/tar",
+            "--use-compress-program", "$binDir/zstd",
             "-cf", outputPath, "-C", backupDir)
         args.addAll(backupFiles)
         args.add("-C"); args.add(microMsgDir)
         args.addAll(attDirs)
         return execCommand(args.toTypedArray(), arrayOf(
-            "LD_LIBRARY_PATH=/data/data/com.termux/files/usr/lib", "ZSTD_CLEVEL=3"))
+            "LD_LIBRARY_PATH=$binDir", "ZSTD_CLEVEL=3"))
     }
 }
